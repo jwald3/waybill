fddbf9f056da19b5d4e1f2b90429a1f4a5627450
Jordan <jwald3@brockport.edu>
2025-01-04 22:38:56 -0500

    Added comments to handler.go

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,12 +2,12 @@ package handler
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
+// a custom response format you can use in your own handlers. Successful operations can use the `Message` property
+// while unsuccessful operations can use the `Error` property
 type Response struct {
 	Status  int    `json:"status"`
 	Data    any    `json:"data,omitempty"`
@@ -15,40 +15,23 @@ type Response struct {
 	Message string `json:"message,omitempty"`
 }
 
+// a general-purpose function you can use to package data into a JSON response.
+// use this in your resource-specific handlers to package the data you want to send in responses
 func WriteJSON(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(data)
 }
 
+// takes a request (r) and attempts to decode it into the shape defined in v
+// you can use this in your resource-specific handlers to ensure that you're getting data that matches
+// the domain model (or DTO)
 func ReadJSON(r *http.Request, v any) error {
 	return json.NewDecoder(r.Body).Decode(v)
 }
 
-func GetIDParam(r *http.Request) (int64, error) {
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) < 3 {
-		return 0, errors.New("invalid path")
-	}
-	return strconv.ParseInt(parts[len(parts)-1], 10, 64)
-}
-
-func GetIDFromPath(path string) (int64, error) {
-	parts := strings.Split(path, "/")
-	if len(parts) == 0 {
-		return 0, errors.New("invalid path")
-	}
-
-	idStr := parts[len(parts)-1]
-
-	id, err := strconv.ParseInt(idStr, 10, 64)
-	if err != nil {
-		return 0, errors.New("invalid ID format")
-	}
-
-	return id, nil
-}
-
+// a parser for integer-based query parameters, such as limit, page, etc.
+// you can use this as a way to help unpack information from a request URL
 func getQueryIntParam(r *http.Request, key string, defaultValue int) int {
 	valStr := r.URL.Query().Get(key)
 	if valStr == "" {
