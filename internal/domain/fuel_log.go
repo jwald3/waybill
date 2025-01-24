9da2f751d206d8d8a740ab0be1ee0274eacd377b
Jordan <jwald3@brockport.edu>
2025-01-24 00:12:32 -0500

    created fuel log constructor

--- internal/domain/fuel_log.go
+++ internal/domain/fuel_log.go
@@ -1,6 +1,10 @@
 package domain
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type FuelLog struct {
 	ID               primitive.ObjectID `bson:"_id,omitempty"`
@@ -14,4 +18,29 @@ type FuelLog struct {
 	OdometerReading  int                `bson:"odometer_reading"`
 	CreatedAt        primitive.DateTime `bson:"created_at"`
 	UpdatedAt        primitive.DateTime `bson:"updated_at"`
+}
+
+func NewFuelLog(
+	truckId,
+	driverId primitive.ObjectID,
+	date,
+	location string,
+	gallonsPurchased,
+	pricePerGallon,
+	totalCost float64,
+	odometerReading int) (*FuelLog, error) {
+	now := time.Now()
+
+	return &FuelLog{
+		TruckID:          truckId,
+		DriverID:         driverId,
+		Date:             date,
+		GallonsPurchased: gallonsPurchased,
+		PricePerGallon:   pricePerGallon,
+		TotalCost:        totalCost,
+		Location:         location,
+		OdometerReading:  odometerReading,
+		CreatedAt:        primitive.NewDateTimeFromTime(now),
+		UpdatedAt:        primitive.NewDateTimeFromTime(now),
+	}, nil
 }
