d47d405afa544021fa76f8b8207bd7f60ca4d0e6
Jordan <jwald3@brockport.edu>
2025-01-24 00:08:30 -0500

    created constructor for maintenance logs

--- internal/domain/maintenance_logs.go
+++ internal/domain/maintenance_logs.go
@@ -1,6 +1,10 @@
 package domain
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type MaintenanceLog struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"`
@@ -13,4 +17,28 @@ type MaintenanceLog struct {
 	Location    string             `bson:"location"`
 	CreatedAt   primitive.DateTime `bson:"created_at"`
 	UpdatedAt   primitive.DateTime `bson:"updated_at"`
+}
+
+func NewMaintenanceLog(
+	truckId primitive.ObjectID,
+	date,
+	serviceType,
+	notes,
+	mechanic,
+	location string,
+	cost float64) (*MaintenanceLog, error) {
+
+	now := time.Now()
+
+	return &MaintenanceLog{
+		TruckID:     truckId,
+		Date:        date,
+		ServiceType: serviceType,
+		Cost:        cost,
+		Notes:       notes,
+		Mechanic:    mechanic,
+		Location:    location,
+		CreatedAt:   primitive.NewDateTimeFromTime(now),
+		UpdatedAt:   primitive.NewDateTimeFromTime(now),
+	}, nil
 }
