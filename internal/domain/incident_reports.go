e9ef843303e4da0317a4c0ce24caca9743c6ee58
Jordan <jwald3@brockport.edu>
2025-01-24 00:16:26 -0500

    created incident report constructor

--- internal/domain/incident_reports.go
+++ internal/domain/incident_reports.go
@@ -1,6 +1,10 @@
 package domain
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type IncidentReport struct {
 	ID             primitive.ObjectID `bson:"_id,omitempty"`
@@ -14,4 +18,21 @@ type IncidentReport struct {
 	DamageEstimate float64            `bson:"damage_estimate"`
 	CreatedAt      primitive.DateTime `bson:"created_at"`
 	UpdatedAt      primitive.DateTime `bson:"updated_at"`
+}
+
+func NewIncidentReport(trip, truckId, driverId primitive.ObjectID, incidentType, description, date, location string, damageEstimate float64) (*IncidentReport, error) {
+	now := time.Now()
+
+	return &IncidentReport{
+		Trip:           trip,
+		TruckID:        truckId,
+		DriverID:       driverId,
+		Type:           incidentType,
+		Description:    description,
+		Date:           date,
+		Location:       location,
+		DamageEstimate: damageEstimate,
+		CreatedAt:      primitive.NewDateTimeFromTime(now),
+		UpdatedAt:      primitive.NewDateTimeFromTime(now),
+	}, nil
 }
