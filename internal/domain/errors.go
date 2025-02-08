090343c841618058d4e82c27f5fffcaf7a9c1e9e
Jordan <jwald3@brockport.edu>
2025-02-08 18:30:48 -0500

    added specific error types

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,3 +3,9 @@ package domain
 import "errors"
 
 var ErrDriverNotFound = errors.New("driver not found")
+var ErrFacilityNotFound = errors.New("facility not found")
+var ErrFuelLogNotFound = errors.New("fuel log not found")
+var ErrIncidentReportNotFound = errors.New("incident report not found")
+var ErrMaintenanceLogNotFound = errors.New("maintenance log not found")
+var ErrTripNotFound = errors.New("trip not found")
+var ErrTruckNotFound = errors.New("truck not found")
