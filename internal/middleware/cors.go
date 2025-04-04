3ef47e0b29fa86369cd95f62bca751e2fc3388ba
Jordan <jwald3@brockport.edu>
2025-04-04 10:20:48 -0400

    updated cors middleware policy

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,7 +4,23 @@ import "net/http"
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // TODO: make sure to set this to a more restrictive setting
+		// Check the origin of the request
+		origin := r.Header.Get("Origin")
+
+		allowedOrigins := []string{
+			"https://getwaybill.com",
+			"https://www.getwaybill.com",
+			"http://localhost:5173", // Keep for local development
+		}
+
+		// Set appropriate CORS headers based on origin
+		for _, allowed := range allowedOrigins {
+			if origin == allowed {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+				break
+			}
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
